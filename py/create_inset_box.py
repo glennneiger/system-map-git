78ae2d4cd7c9921a392d048bf7cf319dc35b7759
grant-humphries <grant.humphries@gmail.com>
2015-05-22 16:25:20 -0700

    added carto POIs to system map gdb

---
+++ py/create_inset_box.py
@@ -0,0 +1,71 @@
+# (c) Grant Humphries for TriMet, 2015
+# ArcGIS Version:   10.3.0
+# Python Version:   2.7.8
+#--------------------------------
+
+import os
+import re
+import arcpy
+from arcpy import da
+from arcpy import env
+from arcpy import mapping
+from arcpy import management
+
+# Configure environment settings
+env.overwriteOutput = True
+env.workspace = '//gisstore/gis/PUBLIC/GIS_Projects/System_Map/2015'
+
+city_center_bkmk = 'city center extent'
+city_center_mxd = os.path.join(env.workspace, 'mxd', 'city_center_2015.mxd')
+inset_box = os.path.join(env.workspace, 'shp', 'inset_box.shp')
+
+def getBookmarkBbox(mxd_path, bkmk_name):
+	"""Get the coordinates of the city center map's bounding box"""
+
+	bkmk_dict = {}
+	mxd = mapping.MapDocument(mxd_path)
+	for bkmk in arcpy.mapping.ListBookmarks(mxd, bkmk_name):
+		bkmk_dict['x-min'] = bkmk.extent.XMin
+		bkmk_dict['y-min'] = bkmk.extent.YMin
+		bkmk_dict['x-max'] = bkmk.extent.XMax
+		bkmk_dict['y-max'] = bkmk.extent.YMax
+
+	return bkmk_dict
+
+def createInsetBox():
+	"""The bus mall inset covers a portion of the city center map so that
+	needs to be reflected in the inset box, using the inflection point and the
+	city center bound box create an fc that contains the inset box"""
+	
+	inflect_pt = {'x': 7649075, 'y': 686384}
+	bkmk_dict = getBookmarkBbox(city_center_mxd, city_center_bkmk)
+
+	geom_type = 'POLYGON'
+	oregon_spn = arcpy.SpatialReference(2913)
+	management.CreateFeatureclass(os.path.dirname(inset_box), 
+		os.path.basename(inset_box), geom_type, spatial_reference=oregon_spn)
+
+	f_name, f_type = 'name', 'TEXT'
+	management.AddField(inset_box, f_name, f_type)
+
+	drop_field = 'Id'
+	arcpy.management.DeleteField(inset_box, drop_field)
+
+	i_fields = ['Shape@', f_name]
+	i_cursor = da.InsertCursor(inset_box, i_fields)
+
+	ap_array = arcpy.Array()
+	ap_array.add(arcpy.Point(bkmk_dict['x-min'], bkmk_dict['y-min']))
+	ap_array.add(arcpy.Point(bkmk_dict['x-min'], bkmk_dict['y-max']))
+	ap_array.add(arcpy.Point(bkmk_dict['x-max'], bkmk_dict['y-max']))
+	ap_array.add(arcpy.Point(bkmk_dict['x-max'], inflect_pt['y']))
+	ap_array.add(arcpy.Point(inflect_pt['x'], inflect_pt['y']))
+	ap_array.add(arcpy.Point(inflect_pt['x'], bkmk_dict['y-min']))
+	# add first point again to close polygon
+	ap_array.add(arcpy.Point(bkmk_dict['x-min'], bkmk_dict['y-min']))
+
+	i_cursor.insertRow((arcpy.Polygon(ap_array), 'Portland City Center'))
+
+	del i_cursor
+
+createInsetBox()
--- py/get_bookmark_bounding_box.py
+++ py/get_bookmark_bounding_box.py
@@ -6,15 +6,14 @@
 import os
 import re
 import arcpy
+from arcpy import da
 from arcpy import env
 from arcpy import mapping
 from arcpy import management
 
 # Configure environment settings
+env.overwriteOutput = True
 env.workspace = '//gisstore/gis/PUBLIC/GIS_Projects/System_Map/2015'
-
-city_center_bkmk = 'city center extent'
-city_center_mxd = os.path.join(env.workspace, 'mxd', 'city_center_2015.mxd')
 
 def getBookmarkBbox(mxd_path, bkmk_name):
 	"""Create a polygon from based on the city center bookmark and add it to
@@ -30,4 +29,46 @@ def getBookmarkBbox(mxd_path, bkmk_name):
 		print 'Y-max: {0}'.format(bkmk.extent.YMax)
 		print '--------------------------'
 
-getBookmarkBbox(city_center_mxd, city_center_bkmk)
+
+def createBoundingBoxPolygon(mxd_path, bkmk_name, out_fc):
+	"""Create a polygon that from the coordinates of a bookmark bounding box"""
+
+	geom_type = 'POLYGON'
+	oregon_spn = arcpy.SpatialReference(2913)
+	management.CreateFeatureclass(os.path.dirname(out_fc),
+		os.path.basename(out_fc), geom_type, spatial_reference=oregon_spn)
+
+	name_field, f_type = 'name', 'TEXT'
+	management.AddField(out_fc, name_field, f_type)
+
+	# drop defualt field
+	drop_field = 'Id'
+	management.DeleteField(out_fc, drop_field)
+
+	i_fields = ['Shape@', name_field]
+	i_cursor = da.InsertCursor(out_fc, i_fields)
+
+	mxd = mapping.MapDocument(mxd_path)
+	for bkmk in arcpy.mapping.ListBookmarks(mxd, bkmk_name):
+		extent = bkmk.extent
+		pt_array = arcpy.Array()
+
+		pt_array.add(arcpy.Point(extent.XMin, extent.YMin))
+		pt_array.add(arcpy.Point(extent.XMin, extent.YMax))
+		pt_array.add(arcpy.Point(extent.XMax, extent.YMax))
+		pt_array.add(arcpy.Point(extent.XMax, extent.YMin))
+		# add first point again to close polygon
+		pt_array.add(arcpy.Point(extent.XMin, extent.YMin))
+
+		i_cursor.insertRow((arcpy.Polygon(pt_array), bkmk.name))
+
+	del i_cursor
+
+# city_center_bkmk = 'city center extent'
+# city_center_mxd = os.path.join(env.workspace, 'mxd', 'city_center_2015.mxd')
+#getBookmarkBbox(city_center_mxd, city_center_bkmk)
+
+system_map_bkmk = '*system map extent*'
+system_map_mxd = os.path.join(env.workspace, 'mxd', 'system_map_2015.mxd')
+system_map_bbox = os.path.join(env.workspace, 'shp', 'system_map_bbox.shp')
+createBoundingBoxPolygon(system_map_mxd, system_map_bkmk, system_map_bbox)
